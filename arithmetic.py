c3a5099e4922d0997a88c8419265873ee29fa7f7
Delta Student <mswapnali935@gmail.com>
2025-11-10 17:16:49 +0530

    Added python files

---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+# a=10;
+# b=2;
+# print("Addition is:",a+b);
+# print("Subtraction is:",a-b);
+# print("Multiplication is:",a*b);
+# print("Division is:",a/b);
+
+
+a=int(input("enter a :"));
+b=int(input("enter b :"));
+print("Addition is:",a+b);
+print("Subtraction is:",a-b);
+print("Multiplication is:",a*b);
+print("Division is:",a/b);
+# print("Comparison "a>b);
